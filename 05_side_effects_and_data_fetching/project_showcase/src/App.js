6f73f161db01f97ef8b4e0924579b47d9ac22a6a
stephenlambertfs <stephenolambert@gmail.com>
2023-07-10 11:39:45 -0600

    added solution code

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,18 +1,29 @@
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
-import {useState} from "react"
-
+import {useState, useEffect} from "react"
+import ReactDOM from "react-dom"
 const App = () => {
   const [projects, setProjects] = useState([])
   const [isDarkMode, setIsDarkMode] = useState(true)
   const [searchQuery, setSearchQuery] = useState("")
 
-  function handleClick(){
+  const handleUnmount = () => {
+    ReactDOM.unmountComponentAtNode(document.getElementById("root"));
+  }
+
+  console.log("Rendering App Component!")
+
+  useEffect(() => {
+    console.log("Running the use effect hook!")
     fetch("http://localhost:4000/projects")
     .then((res) => res.json())
     .then((proj) => setProjects(proj))
-  }
+
+    return () => {
+      console.log("Cleanup function executed!")
+    }
+  }, [])
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode)
 
@@ -33,7 +44,7 @@ const App = () => {
     <div className="App">
       <Header isDarkMode = {isDarkMode} onToggleDarkMode = {onToggleDarkMode}/>
       <ProjectForm onAddProject = {onAddProject}/>
-      <button onClick={handleClick}>Load Projects</button>
+      <button onClick={handleUnmount}>Unmount</button>
       <ProjectList searchQuery = {searchQuery} projects={projects} handleSearch={handleSearch} setProjects = {setProjects}/>
     </div>
   );
