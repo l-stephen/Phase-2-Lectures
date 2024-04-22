fda3ee41b4310edb0156ce3ed79d197b4afb3fe9
stephenlambertfs <stephenolambert@gmail.com>
2024-04-22 10:00:07 -0600

    Revert "updated starter code"
    This reverts commit 5ea15a9029a77e955c6377c1c447722311901516.

--- 01_components_and_props/project_showcase/src/App.js
+++ 01_components_and_props/project_showcase/src/App.js
@@ -1,16 +1,16 @@
-import Header from "./Components/Header"
-import ProjectForm from "./Components/ProjectForm"
-import ProjectList from "./Components/ProjectList";
-import projects from "./projects";
+import Header from "./components/Header"
+import ProjectForm from "./components/ProjectForm"
+import ProjectList from "./components/ProjectList"
+import projects from "./projects"
 function App() {
+  console.log(projects)
   return (
   <div className="App">
-    <Header/>
+    <Header />
     <ProjectForm />
     <ProjectList projects = {projects}/>
-  </div>
-
-  )
+   </div>
+  );
 }
 
 export default App;
